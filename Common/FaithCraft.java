8e7873635170960b8e126a7719b340ab9992b3a9
KingCreeperEli <ess0523@gmail.com>
2013-07-13 09:57:08 -0400

    Added the base class and the base folders

---
+++ Common/FaithCraft.java
@@ -0,0 +1,10 @@
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "FaithCraft", name = "FaithCraft", version = "1.0 Alpha")
+public class FaithCraft {
+    
+    public FaithCraft() {
+        
+    }
+    
+}
