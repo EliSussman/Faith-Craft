3cb900326f7dae287838fb07e11118ce718573d0
KingCreeperEli <ess0523@gmail.com>
2013-07-13 12:46:32 -0400

    Added Initialization Events to Main Class

--- Common/FaithCraft.java
+++ Common/FaithCraft.java
@@ -1,10 +1,29 @@
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
 
 @Mod(modid = "FaithCraft", name = "FaithCraft", version = "1.0 Alpha")
+@NetworkMod( channels = {""}, clientSideRequired = true, serverSideRequired = true )
+
 public class FaithCraft {
     
-    public FaithCraft() {
-        
+    @EventHandler
+    public static void preInit( FMLPreInitializationEvent event ) {
+
+    }
+
+    @EventHandler
+    public static void init( FMLInitializationEvent event ) {
+
+    }
+
+    @EventHandler
+    public static void postInit( FMLPostInitializationEvent event ) {
+
     }
     
 }
