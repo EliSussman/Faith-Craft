f16947d1e47339f3b3e3ee0fb86bc12943849c7d
DeathSeeker512 <deathseeker152@gmail.com>
2013-07-29 14:33:00 -0500

    Ummm Don't know why but it says there is a difference..

--- Common/common/blocks/Altar.java
+++ Common/common/blocks/Altar.java
@@ -1,8 +1,12 @@
 package common.blocks;
 
+import java.util.Random;
+
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 public class Altar extends BlockContainer {
@@ -15,9 +19,50 @@ public class Altar extends BlockContainer {
     public TileEntity createNewTileEntity(World world) {
         return null;
     }
-    
-    public int quantityDropped(){
+
+    @Override
+    public int quantityDropped(Random random) {
         return 1;
+    }
+
+    public int getRenderType() {
+        return 0;
+    }
+
+    public boolean isOpaqueCube() {
+        return true;
+    }
+
+    public boolean renderAsNormalBlock() {
+        return false;
+    }
+
+    public int damageDropped(int par1) {
+        return par1;
+    }
+
+    public boolean blockEventRecieved(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer) {
+
+        int p = MathHelper.floor_double((double) ((par5EntityPlayer.rotationYaw * 4F) / 360F) + 0.5D) & 3;
+
+        byte byte0 = 3;
+
+        if (p == 0) {
+            byte0 = 4;
+        }
+        if (p == 1) {
+            byte0 = 3;
+        }
+        if (p == 2) {
+            byte0 = 2;
+        }
+        if (p == 3) {
+            byte0 = 1;
+        }
+
+        par1World.setBlockMetadataWithNotify(par2, par3, par4, byte0, p);
+
+        return true;
     }
 
 }
--- Common/common/blocks/models/ModelAltar.java
+++ Common/common/blocks/models/ModelAltar.java
@@ -1,44 +1,43 @@
 package common.blocks.models;
 
+import java.util.List;
+
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.World;
 
-public class ModelAltar extends ModelBase
-{
-  //fields
+public class ModelAltar extends ModelBase {
+    // fields
     ModelRenderer Shape1;
-  
-  public ModelAltar()
-  {
-    textureWidth = 96;
-    textureHeight = 48;
-    
-      Shape1 = new ModelRenderer(this, 0, 0);
-      Shape1.addBox(0F, 0F, 0F, 16, 16, 32);
-      Shape1.setRotationPoint(0F, 0F, 0F);
-      Shape1.setTextureSize(96, 48);
-      Shape1.mirror = true;
-      setRotation(Shape1, 0F, 1.570796F, 0F);
-  }
-  
-  public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-  {
-    super.render(entity, f, f1, f2, f3, f4, f5);
-    setRotationAngles(f, f1, f2, f3, f4, f5);
-    Shape1.render(f5);
-  }
-  
-  private void setRotation(ModelRenderer model, float x, float y, float z)
-  {
-    model.rotateAngleX = x;
-    model.rotateAngleY = y;
-    model.rotateAngleZ = z;
-  }
-  
-  public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5)
-  {
-    super.setRotationAngles(f, f1, f2, f3, f4, f5, null);
-  }
+
+    public ModelAltar() {
+        textureWidth = 96;
+        textureHeight = 48;
+
+        Shape1 = new ModelRenderer(this, 0, 0);
+        Shape1.addBox(0F, 0F, 0F, 16, 16, 32);
+        Shape1.setRotationPoint(0F, 0F, 0F);
+        Shape1.setTextureSize(96, 48);
+        Shape1.mirror = true;
+        setRotation(Shape1, 0F, 1.570796F, 0F);
+    }
+
+    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+        super.render(entity, f, f1, f2, f3, f4, f5);
+        setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+        Shape1.render(f5);
+    }
+
+    private void setRotation(ModelRenderer model, float x, float y, float z) {
+        model.rotateAngleX = x;
+        model.rotateAngleY = y;
+        model.rotateAngleZ = z;
+    }
+
+    public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity) {
+        super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+    }
 
 }
