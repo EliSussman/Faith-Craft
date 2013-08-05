62edce97ef229ab9253ad9edcb1c9a57da0ef143
KingCreeperEli <ess0523@gmail.com>
2013-08-05 19:41:40 -0400

    Added Glowing Effect to Holy Items

--- Common/common/items/Quran.java
+++ Common/common/items/Quran.java
@@ -2,9 +2,8 @@ package common.items;
 
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.item.Item;
-
+import net.minecraft.item.ItemStack;
 import common.Reference;
-
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -21,6 +20,11 @@ public class Quran extends Item {
     @Override
     public void registerIcons(IconRegister register) {
         this.itemIcon = register.registerIcon(Reference.modid + ":" + (this.getUnlocalizedName().substring(5)));
+    }
+    
+    @Override
+    public boolean hasEffect(ItemStack par1ItemStack){
+        return true;
     }
 
 }
